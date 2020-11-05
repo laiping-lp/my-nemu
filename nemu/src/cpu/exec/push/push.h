9b3efb1886b1fbf116b4dd66f2fe212cb9bf4903
tracer-ics2016 <tracer@njuics.org>
2020-11-05 12:48:03 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:48:03 up 5 days,  9:57,  1 user,  load average: 0.46, 0.32, 0.15 bff14820199f4500fcbab1978356af84a5a760ba

---
+++ nemu/src/cpu/exec/push/push.h
@@ -0,0 +1,13 @@
+#ifdef _PUSH_H
+#define _PUSH_H
+
+make_helper(push_r_b);
+make-helper(push_r_v);
+
+make_helper(push_rm_b);
+make_helper(push_em_v);
+
+make_helper(push_i_b);
+make_helper(push_i_v);
+
+#endif
