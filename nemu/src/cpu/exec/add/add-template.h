f02afbc455bb6b7db619193feb72ad7afb56614b
tracer-ics2016 <tracer@njuics.org>
2020-11-13 01:20:16 +0000

    >  compile NEMU 3019244403 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:20:16 up  9:37,  1 user,  load average: 0.03, 0.11, 0.37 aad453f991e136e128b9d599745c97ddca16d8f2

---
+++ nemu/src/cpu/exec/add/add-template.h
@@ -0,0 +1,36 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+static void do_execute () {
+	DATA_TYPE temp = op_dest -> val + op_src -> val;
+	int len = (DATA_BYTE << 3) - 1; //DATA_BYTE * 8 -1
+    cpu.CF = (temp < op_dest -> val); 
+    //sign flag:set equal to high-order bit of result(0 is positive)
+    cpu.SF = temp >> len;
+    //zero flag:set if result is zero,clear otherwise 
+    cpu.ZF = !temp;
+    
+    int t1,t2;
+    t1 = op_dest -> val >> len;
+    t2 = op_src -> val >> len;
+    cpu.OF = (t1 == t2 && t1 != cpu.SF);//different sign && s2 &res different
+    //parity flag:set on low-order eight bits of result contain an even number of 1 bits;cleared otherwise
+    OPERAND_W(op_dest, temp);//write in the res
+    temp ^= temp >> 4;
+    temp ^= temp >> 2;
+    temp ^= temp >> 1;
+    cpu.PF = !(temp & 1);
+	print_asm_template2();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
